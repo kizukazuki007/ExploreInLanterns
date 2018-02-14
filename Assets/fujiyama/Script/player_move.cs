c721b8b28bc5eb421d0765667425d1fb37559ae5
fujiyama <nkc17.hiroki.fujiyama@gmail>
2018-02-14 11:25:59 +0900

    さっさとしてよ

---
+++ Assets/fujiyama/Script/player_move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_move : MonoBehaviour {
+    public float speed=0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Horizontal")==1)
+        {
+            transform.Translate(speed * Time.deltaTime,0,0);
+        }
+
+        if (Input.GetAxis("Horizontal")==-1)
+        {
+            transform.Translate(-speed * Time.deltaTime,0, 0);
+        }
+
+        if (Input.GetAxis("Vertical")==1)
+        {
+            transform.Translate(0,speed * Time.deltaTime,0);
+        }
+
+        if (Input.GetAxis("Vertical")==-1)
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+    }
+}
