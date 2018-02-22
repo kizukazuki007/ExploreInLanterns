167459b74dd9973209567aaadadfa6e6a1740ac6
Imamura_Kazuki <kizukazuki007@gmail.com>
2018-02-22 10:32:10 +0900

    2/22

---
+++ Assets/imamura/Anemy/AnemyMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnemyMove : MonoBehaviour {
+    public Camera camera;
+    int distance=10;
+    float speed = 1;
+	void Start ()
+    {
+	
+	}
+
+    void Update ()
+    {
+
+        Vector3 playerVector = camera.transform.position-transform.position;
+        transform.position += playerVector.normalized * speed * Time.deltaTime;         //normalized
+	}
+}
+//HP ダメージを受ける。　受けた時にスピードを遅くする
+        //if ((transform.position.x < camera.transform.position.x + distance && transform.position.x > camera.transform.position.x - distance)&& (transform.position.y < camera.transform.position.y + distance && transform.position.y > camera.transform.position.y - distance))
+        //{
+        //    attack = true;
+        //}
+        //else
+        //{
+        //    attack = false;
+        //}
